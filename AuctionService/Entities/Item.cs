e28ce0c1c820b85cc8c9bd939313822b0177fadd
Steve Yang <steve.yang@velvix.com>
2023-10-05 20:54:10 +1100

    End of section 2

---
+++ AuctionService/Entities/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuctionService.Entities;
+
+[Table("Items")]	// Specify what database name we want in Table attribute
+public class Item
+{
+	public Guid Id { get; set; }
+
+	public string Make { get; set; }
+
+	public string Model { get; set; }
+
+	public int Year { get; set; }
+
+	public string Color { get; set; }
+
+	public int Mileage { get; set; }
+
+	public string ImageUrl { get; set; }
+
+	// nav properties
+	public Auction Auction { get; set; }
+
+	public Guid AuctionId { get; set; }
+}
